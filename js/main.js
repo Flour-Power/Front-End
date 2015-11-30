de8a35b484f7b23b71808c15d859ea4899836bf8
Hadiya Hope-Crichlow <hopecrichlow@live.com>
2015-11-30 12:09:53 -0500

    Added foundation 6

--- js/main.js
+++ js/main.js
@@ -1,5 +1,22 @@
+import angular from 'angular';
+
+// Import jQuery & Foundaiton
 import $ from 'jquery';
-import _ from 'underscore';
-import moment from 'moment';
+import 'foundation';
+
+// Start it up
+$(document).foundation();
+
+// Set up a run block on an angular module to help with
+// loading foundation after templates load
+angular
+  .module('app', [])
+  .run( function ($rootScope) {
+    // $viewContentLoaded comes from Ui Router
+    $rootScope.$on('$viewContentLoaded', function (event, data) {
+      $(document).foundation();
+    });
+  })
+;
 
 console.log('Hello, World');
